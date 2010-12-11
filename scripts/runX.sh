a74808f97aa087a04151e6656954f8405f44ee25
Vadim Tkachenko <vadim@percona.com>
2010-12-10 20:17:59 -0800

    Added more stats

--- scripts/runX.sh
+++ scripts/runX.sh
@@ -5,6 +5,7 @@ set -e
 
 #export LD_LIBRARY_PATH=/usr/local/mysql/lib/mysql/
 export LD_LIBRARY_PATH=/usr/local/Percona-Server/lib/mysql/
+MYSQLDIR=/usr/local/mysql
 
 ulimit -c unlimited
 
@@ -20,13 +21,16 @@ RT=10800
 
 ROWS=80000000
 
-#log2="/data/log/"
-log2="$DR/"
+log2="/data/bench/"
+#log2="$DR/"
 
 # restore from backup
 function restore {
 
+mkdir -p $DR
+
 rm -fr $DR/*
+rm -f $log2/ib_log*
 
 echo $log2
 #for nm in ibdata1 ib_logfile0 ib_logfile1
@@ -39,6 +43,7 @@ done
 
 cp -r $BD/mysql $DR
 pagecache-management.sh cp -r $BD/tpcc1000 $DR
+pagecache-management.sh cp -r $BD/ibdata1 $log2
 
 sync
 echo 3 > /proc/sys/vm/drop_caches
@@ -88,16 +93,17 @@ RUN_NUMBER=`expr $RUN_NUMBER + 1`
 echo $RUN_NUMBER > .run_number
 
 #for par in  1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 43
-for par in  13 26 39 52 65 78
+#for par in  13 26 39 52 65 78
 #for par in 39 52 65 78
-#for par in 65 78
+#for par in 13 52 78 144
+for par in 52 144
 do
 
 restore
 
 export OS_FILE_LOG_BLOCK_SIZE=4096
 #/home/yasufumi/opt/mysql-5.5.6_p/bin/mysqld --defaults-file=$CONFIG --datadir=$DR  --innodb_log_group_home_dir=$log2 --innodb_thread_concurrency=0 --innodb-buffer-pool-size=${par}GB &
-/usr/local/mysql/libexec/mysqld --defaults-file=$CONFIG --datadir=$DR  --innodb_log_group_home_dir=$log2 --innodb_thread_concurrency=0 --innodb-buffer-pool-size=${par}GB &
+/usr/local/mysql/libexec/mysqld --defaults-file=$CONFIG --datadir=$DR --innodb_data_home_dir=$log2 --innodb_log_group_home_dir=$log2 --innodb_thread_concurrency=0 --innodb-buffer-pool-size=${par}GB &
 #/usr/local/mysql/bin/mysqld --defaults-file=$CONFIG --datadir=$DR  --innodb_log_group_home_dir=$log2 --innodb_thread_concurrency=0 --innodb-buffer-pool-size=${par}GB --innodb-buffer-pool-instances=8 &
 
 MYSQLPID=$!
@@ -128,11 +134,15 @@ vmstat 5 2000 >> $OUTDIR/vmstat.${par}res &
 PIDV=$!
 ./virident_stat.sh >> $OUTDIR/virident.${par}res &
 PIDVS=$!
+$MYSQLDIR/bin/mysqladmin ext -i10 -r >> $OUTDIR/mysqladminext.${par}res &
+PIDMYSQLSTAT=$!
+
 
 cp $CONFIG $OUTDIR
 cp $0 $OUTDIR
 mysqladmin variables >>  $OUTDIR/mysql_variables.res
 ./tpcc_start localhost tpcc1000 root "" 1000 24 10 3600 | tee -a $OUTDIR/tpcc.${par}.out
+kill $PIDMYSQLSTAT
 kill -9 $PID
 kill -9 $PIDV
 kill -9 $PIDVS
