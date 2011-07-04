7ad3f5aecacd35ce4fe8cd2bd06cc97a95c2e53a
root <root@hppro1.office.percona.com>
2011-07-04 15:46:24 -0700

    Changed load.sh script to load N warehouses in parallel

--- load.sh
+++ load.sh
@@ -1,7 +1,8 @@
 export LD_LIBRARY_PATH=/usr/local/mysql/lib/mysql/
-DBNAME=tpcc1000_8k
-./tpcc_load localhost $DBNAME root "" 1000 1 1 1000  >> 1.out &
-./tpcc_load localhost $DBNAME root "" 1000 2 1 1000  >> 2.out &
-./tpcc_load localhost $DBNAME root "" 1000 3 1 1000  >> 3.out &
-./tpcc_load localhost $DBNAME root "" 1000 4 1 500  >> 4_1.out &
-./tpcc_load localhost $DBNAME root "" 1000 4 501 1000 >> 4_2.out &
+DBNAME=$1
+WH=$2
+./tpcc_load localhost $DBNAME root "" $WH 1 1 $WH >> 1.out &
+./tpcc_load localhost $DBNAME root "" $WH 2 1 $WH  >> 2.out &
+./tpcc_load localhost $DBNAME root "" $WH 3 1 $WH  >> 3.out &
+./tpcc_load localhost $DBNAME root "" $WH 4 1 $(($WH/2))  >> 4_1.out &
+./tpcc_load localhost $DBNAME root "" $WH 4 $(($WH/2+1)) $WH >> 4_2.out &
